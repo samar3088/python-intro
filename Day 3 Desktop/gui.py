35bcc5c6d26700f436522bd9968f4323f6d0723d
samar.s <samar@3088>
2025-08-18 06:48:56 +0530

    Edit and Complete Buttons done

--- Day 3 Desktop/gui.py
+++ Day 3 Desktop/gui.py
@@ -12,10 +12,13 @@ list_box = sg.Listbox(values=functions.get_todos(),
                       key="todos", enable_events=True,
                       size=[45,10])
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 
 window = sg.Window('My TODO Window',
                    layout=[[label], [input_box, add_button],
-                           [list_box, edit_button]],
+                           [list_box, edit_button, complete_button],
+                           [exit_button]],
                    font=('Helvetica',12))
 
 while True:
@@ -44,6 +47,17 @@ while True:
 
         case "todos":
             window['todo'].update(value=values['todos'][0].strip())
+
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value="")
+
+        case "Exit":
+            break
 
         case sg.WIN_CLOSED:
             break
